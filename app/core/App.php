6cbe2863815261a3e67a1ea973f5fe908f200b80
AsiaSaja <147909222+AsiaSaja@users.noreply.github.com>
2024-10-11 21:08:39 +0700

    first commit

--- app/core/App.php
+++ app/core/App.php
@@ -24,7 +24,7 @@ class App {
         // method
         if (isset($url[1])){
             if (method_exists($this->controller, $url[1])) {
-                $this->method -> $url[1];
+                $this-> method -> $url[1];
                 unset($url[1]);
             }
         }
