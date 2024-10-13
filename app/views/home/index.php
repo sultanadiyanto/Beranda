15881bd5d128c3fd719d84ed1a7cef3b28eb5c30
AsiaSaja <lukmanpakiki@gmail.com>
2024-10-13 17:38:17 +0700

    forth commit (asset)

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,10 @@
 
-<body>
-    <h1>Selamat datang di web saya</h1>
+<div class="container">
+    <div class="container mt-4">
+        <h2>Selamat Datang di web saya</h2>
+        <div class="mt-4 p-5 bg-primary text-white rounded">
+            <h1>Jumbotron Example</h1> 
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..</p> 
+        </div>
+    </div>
+</div>
--- app/views/kompetisi/index.php
+++ app/views/kompetisi/index.php
@@ -1,3 +1,5 @@
-
-    <h1>Ini Index Kompetisi</h1>
+<div class="container">
+    <h1 class="mt-4">Ini Index Kompetisi</h1>
+    <img src="<?= BASEURL;?>/img/GSC_Polindra.jpeg" alt="GSC" class="shadow">
     <p>Halo nama saya <?= $data['nama']; ?>, saya istri <?= $data['istri']; ?></p>
+</div>
